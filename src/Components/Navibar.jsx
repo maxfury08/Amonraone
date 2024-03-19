4ba70744e01ea003541b52c8d9169c0d61d6bd86
maxfury08 <38.bcs.0008@gmail.com>
2024-03-19 19:04:52 +0530

    first commit

---
+++ src/Components/Navibar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar } from 'flowbite-react';
+// import Link from 'next/link';
+
+const Navibar = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Navibar
